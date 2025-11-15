74405028d269a445d4a7c9f159a30616d56cdb58
Yatogaii <781023956@qq.com>
2025-11-15 14:49:38 +0800

    feat(RAG): add RAG abstract class.

---
+++ src/rag/retriever.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+@dataclass
+class Chunk:
+    content: str
+    metadata: dict
+    score:float
+
+class RAG(ABC):
+    @abstractmethod
+    def query_relevant_documents(self, query: str):
+        pass
+
+    @abstractmethod
+    def list_resources(self):
+        pass
